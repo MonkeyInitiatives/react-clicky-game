4b755ddacbcb19eae3c0c2db770fe804d402b9f1
Thomas Woods <twoods@MacBook-Pro.local>
2019-04-02 14:58:10 -0400

    Updates

---
+++ precache-manifest.663c842e798ac6c9d45db34072ff4ccd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  },
+  {
+    "revision": "a40a79ce54d114d80759",
+    "url": "/react-clicky-game/static/js/main.bc29b07b.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-clicky-game/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a40a79ce54d114d80759",
+    "url": "/react-clicky-game/static/css/main.55e7ec8d.chunk.css"
+  },
+  {
+    "revision": "5fcfa5ba90d2f111837642a62581e215",
+    "url": "/react-clicky-game/index.html"
+  }
+];
