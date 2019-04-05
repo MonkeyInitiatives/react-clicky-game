0b145e301f736e680a8f8c4a39407fe65a4c45ef
Thomas Woods <>
2019-04-04 23:03:02 -0400

    media query remove background color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-dark bg-primary">
                     <a className="navbar-brand" href="/react-clicky-game">Clicky Game</a>
-                    <span className="navbar-brand">
+                    {/* <span className="navbar-brand">
                         Click an image to begin!
-                    </span>
+                    </span> */}
                     <span className="navbar-brand" id="game-scores">
                         Score: 0 | Top Score: 0
                     </span>
