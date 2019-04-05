d1cb6f8544c4307a04ed962031a1bd609884d6c0
Thomas Woods <>
2019-04-04 23:31:27 -0400

    css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,19 @@ import React from 'react';
 const styles = {
     jumbotron: {
         textAlign: "center",
-        color: "black",
+        color: "white",
+        background: "none",
+        textShadow: "2px 2px #000000"
+      },
+    jumbotronContainer: {
         backgroundImage: "url('http://standardflags.com/shop/wp-content/uploads/2013/01/Nylon-American-Flag-closeup-1.jpg')",
-        backgroundSize: "100% 100%"
-      }
+        background: "cover"
+    }
   };
 
 function Header (){
         return(
-            <div className="jumbotron jumbotron-fluid bg-dark">
+            <div className="jumbotron jumbotron-fluid" style={styles.jumbotronContainer}>
                 <div className="container" style={styles.jumbotron}>
                     <h1 className="display-4">React US Flag Game!</h1>
                     <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
