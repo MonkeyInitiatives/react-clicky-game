1c448451b3e9bf13952460008ff26443e701394a
Thomas Woods <>
2019-04-04 23:05:48 -0400

    css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const styles = {
     jumbotron: {
         textAlign: "center",
         color: "white",
+        backgroundImage: "url('https://media.graytvinc.com/images/810*455/American+Flag44.jpg')"
       }
   };
 
@@ -11,7 +12,7 @@ function Header (){
         return(
             <div className="jumbotron jumbotron-fluid bg-dark">
                 <div className="container" style={styles.jumbotron}>
-                    <h1 className="display-4">Clicky Game!</h1>
+                    <h1 className="display-4">React US Flag Game!</h1>
                     <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
                 </div>
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends Component {
         return(
             <div>
                 <nav className="navbar navbar-dark bg-primary">
-                    <a className="navbar-brand" href="/react-clicky-game">Clicky Game</a>
+                    <a className="navbar-brand" href="/react-clicky-game">React US Flag Game!</a>
                     {/* <span className="navbar-brand">
                         Click an image to begin!
                     </span> */}
